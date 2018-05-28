dd430bdf30a8ef9a8624d1090a90c00aa0c60101
huangpengfei <445412679@qq.com>
2018-05-28 09:06:29 +0800

    增加消息转发功能

---
+++ src/JFClient.java
@@ -0,0 +1,51 @@
+/**
+ * Created by huangpengfei on 2018/5/27.
+ */
+public class JFClient {
+
+    private String username;
+    private String pwd;
+    private String hostName;
+    private String hostAddr;
+    private String ipAddr;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getHostAddr() {
+        return hostAddr;
+    }
+
+    public void setHostAddr(String hostAddr) {
+        this.hostAddr = hostAddr;
+    }
+
+    public String getIpAddr() {
+        return ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        this.ipAddr = ipAddr;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,38 @@ import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
 
         ServerSocket serverSocket = new ServerSocket(30000);
+        int count = 0;
+
+        ArrayList arrayList = new ArrayList();
         while (true){
             final Socket socket = serverSocket.accept();
-            new Thread(){
+            count ++;
+            System.out.println(count + "个链接已经建立");
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("您好，您收到了JAVA服务器的一条消息".getBytes());
 
+            InetAddress address = socket.getInetAddress();
+            String hostName = address.getHostName();
+            String hostAddr = address.getHostAddress();
+            byte[] addr = address.getAddress();
+            JFClient client = new JFClient();
+            client.setHostAddr(hostAddr);
+            client.setHostName(hostName);
+            System.out.println("hostName" + hostName);
+            System.out.println("hostAddr" + hostAddr);
+
+            new Thread(){
                 @Override
                 public void run() {
                     System.out.println("========");
@@ -26,14 +44,14 @@ public class Main {
                         int hasRead = -1;
                         while ((hasRead = inputStream.read(buff)) > 0){
                             System.out.println("接受的数据：" + new String(buff, 0, hasRead, "utf-8"));
+                            outputStream.write("JAVA已经接收到您的信息,返回数据阿拉斯吧".getBytes());
                         }
                     }catch (Exception e){
                         e.printStackTrace();
                     }
                 }
             }.start();
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write("您好，您收到了JAVA服务器的一条消息".getBytes());
+
         }
     }
 }
